52f409ccbd6a3e9c65bf87dad973169f7e94dfc3
KaleShivam05 <shivamkale0510@gmail.com>
2025-05-23 01:24:48 +0530

    Add files via upload

---
+++ JayGanesh.java
@@ -0,0 +1,10 @@
+class JayGanesh
+{
+    public static void main(String A[])
+    {
+        System.out.println("Jay Ganesh...");
+    }
+}
+
+// javac Hello.java
+// java Hello       java Hello.java
