011b9a887e8b1ba7bf89b752518da551a36d1f58
KaleShivam05 <shivamkale0510@gmail.com>
2025-06-11 23:14:32 +0530

    Add files via upload

---
+++ Collections9.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Collections9
+{
+    public static void main(String A[])
+    {
+        Vector <Double> vobj = new Vector <Double> ();
+
+        vobj.add(11.23);
+        vobj.add(21.32);
+        vobj.add(51.45);
+        vobj.add(101.11);
+
+        System.out.println(vobj);
+
+        vobj.remove(2);
+
+        System.out.println(vobj);
+
+
+        Iterator iobj = hobj.iterator();
+
+        while(iobj.hasNext())
+        {
+          System.out.println(iobj.next());
+        }
+
+        vobj.clear();
+
+
+
+    }
+}
